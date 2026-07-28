d4795fddc9a23a5839e184ccd3e41e8101e6f272
somehussar <148253652+somehussar@users.noreply.github.com>
2026-07-28 23:13:00 +0200

    Open source MC font, https://github.com/IdreesInc/Minecraft-Font/releases/tag/v1.0

--- src/main/java/io/github/somehussar/crystalgraphics/harness/util/HarnessFontUtil.java
+++ src/main/java/io/github/somehussar/crystalgraphics/harness/util/HarnessFontUtil.java
@@ -10,7 +10,7 @@ public final class HarnessFontUtil {
     public static final String ARABIC_FONT = resolveFontPath("../CrystalGraphics/core/src/main/resources/assets/crystalgraphics/IBMPlexSansArabic-Regular.ttf");
     public static final String JAPANESE_FONT = resolveFontPath("../CrystalGraphics/core/src/main/resources/assets/crystalgraphics/MPLUS1p-Regular.ttf");
     public static final String LATIN_FONT = resolveFontPath("../CrystalGraphics/core/src/main/resources/assets/crystalgraphics/IBMPlexSans-Regular.ttf");
-    public static final String MINECRAFT_FONT = resolveFontPath("../CrystalGraphics/core/src/main/resources/assets/crystalgraphics/mojangles.ttf");
+    public static final String MINECRAFT_FONT = resolveFontPath("../CrystalGraphics/core/src/main/resources/assets/crystalgraphics/Minecraft.otf");
 
     /**
      * Resolves a font path from the given config, falling back to system font discovery.
